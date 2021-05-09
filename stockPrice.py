afba611fc51150096ad7a06b39ac1ecf288772a5
Juan P. Romano <programming@jpromano.net>
2021-05-09 14:23:07 -0300

    Almost finished

--- stockPrice.py
+++ stockPrice.py
@@ -18,3 +18,13 @@ tickerSymbol = 'GOOGL'
 
 # Get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
+
+# Get the historical prices for this ticker for the last decade
+# Modify the period value if you want a bigger or smaller range
+tickerDf = tickerData.history(period='1d', start='2011-5-9', end='2021-5-9')
+
+# Open High Low Close Volume Dividends Stock Splits
+
+# Now show the volume and close values with streamlit
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
